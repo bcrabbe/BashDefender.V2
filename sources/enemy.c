e5269c41e6894f09405a7559df3e6bb03a6bc311
Benjamin Clarke <benjaminclarke@benjamiarkesmbp.default>
2015-01-06 23:22:00 +0000

    changed char heavy width to 100

--- sources/enemy.c
+++ sources/enemy.c
@@ -406,7 +406,7 @@ void present_enemy(Display d)
         if(!isDead(i))
         {
 			if(e->eType == charBasic || e->eType == charHeavy)	{
-            drawEnemy(e->x, e->y, e->width, e->height, 2331, 254, e->eType, 9, 200);
+            drawEnemy(e->x, e->y, e->width, e->height, 2331, 200, e->eType, 9, 200);
             drawRect(e->x, e->y -20, 0, 0, 80, 10, (double)e->health, (double)e->maxHealth);
 			} else if(e->eType == intBasic || e->eType == intHeavy)	{
 			drawEnemy(e->x, e->y, e->width, e->height, 2010, 121, e->eType, 15, 200);
